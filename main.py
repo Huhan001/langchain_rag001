740b604656d1610990e447d18df23cb910e11d33
Humphrey Hanson <114668148+Huhan001@users.noreply.github.com>
2024-01-19 00:21:39 +0100

    left to bed in between

--- main.py
+++ main.py
@@ -12,6 +12,18 @@ from langchain_core.output_parsers import StrOutputParser
 #- retrievers give access to knowledge by picking the most relevant documents from a large collection of documents
 from langchain_community.document_loaders import WebBaseLoader
 
+#embedings ranked together.
+from langchain_openai import OpenAIEmbeddings
+
+# building out index
+from langchain_community.vectorstores import FAISS 
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+
+#rettiver chain
+from langchain.chains.combine_documents import create_stuff_documents_chain
+
+
+
 def RagApplication():
 
     # load enviroment .env file
@@ -20,10 +32,10 @@ def RagApplication():
     openai_chat = ChatOpenAI(openai_api_key= openai_key)
     
     # chat prompt
-    prompt = ChatPromptTemplate.from_messages([
-        ("system", "you are world class technical documentation writer"),
-         ("user", "{input}")
-    ])
+    # prompt = ChatPromptTemplate.from_messages([
+    #     ("system", "you are world class technical documentation writer"),
+    #      ("user", "{input}")
+    # ])
 
     # chain = prompt | openai_chat
     # print(chain.invoke({"input": "how can langsmith help with testing?"}))
@@ -37,6 +49,31 @@ def RagApplication():
     #retreiver by passing in web and scrapping with beautiful soap
     loader = WebBaseLoader("https://docs.smith.langchain.com/overview")
     docs = loader.load()
+
+    #make sure you have the embedings packages installed
+    embeddings = OpenAIEmbeddings()
+
+    #build index
+    text_splitter = RecursiveCharacterTextSplitter()
+    documents = text_splitter.split_documents(docs)
+    vector = FAISS.from_documents(documents, embeddings)
+
+    #lets set up a chain that takes a question and retrieves documents then generaets an answer.
+    prompt = ChatPromptTemplate.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 if __name__ == "__main__":
     RagApplication()
