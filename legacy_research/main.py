3fe6a0568dac1541cc39ef189267e7bf602189e6
Humphrey Hanson <114668148+Huhan001@users.noreply.github.com>
2024-04-06 23:48:23 +0200

    Clean up

--- legacy_research/main.py
+++ legacy_research/main.py
@@ -8,13 +8,3 @@ if __name__ == "__main__":
     newone = replace_loading_dataset_with_csv_read(run_model(), path)
     exec(newone)
     # finalise("show me the species accoding to bodymass and sex?")
-
-try:
-    # newone = enforce_rules(run_model("what is the price of diamond according to carat and depth?"))
-    # st.code(newone)
-    newone = code_and_test("visualize the relation between price and depth, and carat of diamond?")
-    st.code(newone)
-    # timess = exec(newone)
-    # st.altair_chart(timess, use_container_width=True)
-except AttributeError:
-    pass  # Suppress the AttributeError
