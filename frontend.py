fcdf9b2b98d0a7b155f91c1b10156d2819a9e30e
Humphrey Hanson <114668148+Huhan001@users.noreply.github.com>
2024-04-06 23:41:00 +0200

    using containers

--- frontend.py
+++ frontend.py
@@ -4,13 +4,13 @@ import pandas as pd
 
 
 st.set_option('deprecation.showPyplotGlobalUse', False)
-col = st.columns([0.8, 0.2])
 
-with col[0]:
+
+with st.container():
     data = pd.read_csv("/Users/humphreyhanson/fleet/langchain_rag001/dataset/diamonds.csv")
     st.session_state.dataframe = data
 
-    question = st.text_area(placeholder="Your inquiry goes inhere", value="", label="Write out your question")
+    question = st.text_area(placeholder="Your inquiry goes inhere", value="", label="Write out your question", height=1)
     Vizbutton = st.button("Generate Plot",type="secondary")
 
     if Vizbutton:
